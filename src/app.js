8fa7d99b3d8fcb3188245cbe858a7c6ed9c36b59
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-24 15:49:34 -0500

    debe configurarse handlebars en express para usarse como motor de plantilla y que el backend pueda leer variables y generar archivos html en el navegador. Debe configurarse en el archivo app con app.engine y cambiar la extensión de los views .html a la extensión .hbs. Tener en cuenta las importaciones

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 // const express = require("express")
-
+import exphbs from "express-handlebars";
 import indexRouter from "./routes/index.routes";
 
 const app = express();
 
+// Decirle a express qué motor de plantilla voy a usar
+app.engine(".hbs", exphbs({
+    extname: ".hbs",
+}
+))
+
+// Routes
 app.use(indexRouter);
 
 export default app;
