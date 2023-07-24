b1e0d58434d1ccc8966045f4d15e4f8e29c0c6ee
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-24 15:16:12 -0500

    creado archivo enrutador e importado a la app principal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from "express";
 // const express = require("express")
+import indexRouter from "./routes/index.routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+app.use(indexRouter);
 
 app.listen(5090, () => {
   console.log("Server on port " + 5090);
