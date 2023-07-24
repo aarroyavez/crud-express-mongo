8bc0f12cdf98f0398424c9a949d92e9563f38e88
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-24 15:24:22 -0500

    creado archivo app.js para configurar la app e index.js solo arranca la app o el servidor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-import express from "express";
-// const express = require("express")
-import indexRouter from "./routes/index.routes";
-
-const app = express();
-
-app.use(indexRouter);
+import app from "./app"
 
 app.listen(5090, () => {
   console.log("Server on port " + 5090);
