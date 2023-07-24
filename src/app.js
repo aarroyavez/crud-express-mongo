830d38f1439df5a7988e82bc561b7bfd42ce258f
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-24 16:01:30 -0500

    importar path para que express encuentre la carpeta views dentro de src

--- src/app.js
+++ src/app.js
@@ -2,11 +2,15 @@ import express from "express";
 // const express = require("express")
 import exphbs from "express-handlebars";
 import indexRouter from "./routes/index.routes";
+import path from "path";
+
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"))
 // Decirle a express qué motor de plantilla voy a usar
 app.engine(".hbs", exphbs({
+
     extname: ".hbs",
 }
 ))
