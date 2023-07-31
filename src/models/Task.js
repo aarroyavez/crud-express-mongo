00b4423f3113beba26425a56944ec3670f1c6a71
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-07-30 23:53:54 -0500

    función exportada

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,10 +1,16 @@
 import { Schema, model } from "mongoose";
 
-const task = new Schema({
+const taskSchema = new Schema(
+  {
     title: String,
     description: String,
     done: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-    timestamps: true
-})      
+// A partir del Schema, crear el modelo de datos
+
+export default model("Task", taskSchema);
