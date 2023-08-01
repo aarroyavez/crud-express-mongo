0099ac7d5cc65d9fb841b3e0ca70b20be91850d6
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-08-01 16:23:30 -0500

    rutas de toggleDone y delete ordenadas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -18,15 +18,15 @@ router.post("/tasks/add", createTask);
 //     res.render("about")
 // })
 
+// toggleDone -> toggle = que cambie a true o false
+router.get("/tasks/:id/toggleDone", toggleDone);
+
 router.get("/tasks/:id/edit", renderTaskEdit);
 
 // edit route
 router.post("/tasks/:id/edit", editTask);
 
 // delete router
-router.get("/delete/:id", deleteTask);
-
-// toggleDone -> toggle = que cambie a true o false
-router.get("/toggleDone/:id", toggleDone);
+router.get("/tasks/:id/delete", deleteTask);
 
 export default router;
