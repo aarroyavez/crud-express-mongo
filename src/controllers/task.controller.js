30983800d2634d1273c6ce3683d66e97be930a38
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-08-01 16:06:55 -0500

    controlador de rutas creado. Se modificarán rutas para que sean más descriptivas

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,8 +1,71 @@
+import Task from "../models/Task";
+
+// Mostrar tareas
 export const renderTasks = async (req, res) => {
+  const tasks = await Task.find().lean();
+  // console.log(tasks)
 
-    const tasks = await Task.find().lean()
-    // console.log(tasks)
+  // index hace referencia al hbs
+  res.render("index", { tasks: tasks }); //propiedad tasks y su valor son las tareas que obtengo en la base de datos (tasks)
+};
 
-    // index hace referencia al hbs
-    res.render("index", { tasks: tasks }) //propiedad tasks y su valor son las tareas que obtengo en la base de datos (tasks)
-}
+// Crear tareas
+export const createTask = async (req, res) => {
+  try {
+    const task = Task(req.body);
+
+    // Objeto que se guardara en mongodb
+    // const taskSaved = await task.save()
+    await task.save();
+
+    // console.log(taskSaved)
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Consulta una tarea y luego renderiza un formulario para poderlo editar
+export const renderTaskEdit = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean(); //lean para que admita objetos de js y no de mongodb
+
+    res.render("edit", { task });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// Editar tareas
+export const editTask = async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndUpdate(id, req.body);
+
+  console.log(req.body);
+  console.log(req.params.id);
+
+  res.redirect("/");
+};
+
+// Eliminar Tareas
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  await Task.findByIdAndDelete(id); //no es necesario pasar un body porque no lo va a actualizar. Solo borrará por id
+  res.redirect("/");
+};
+
+export const toggleDone = async (req, res) => {
+  const { id } = req.params;
+
+  const task = await Task.findById(id);
+
+  // Por defecto es false, el símbolo ! lo convierte en true
+  task.done = !task.done;
+
+  // Guardar
+  await task.save();
+
+  res.redirect("/");
+};
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,78 +1,32 @@
 import { Router } from "express";
-import Task from "../models/Task"
-import { renderTasks } from "../controllers/task.controller";
-const router = Router()
+import {
+  renderTasks,
+  createTask,
+  renderTaskEdit,
+  editTask,
+  deleteTask,
+  toggleDone,
+} from "../controllers/task.controller";
 
-router.get("/", renderTasks)
+const router = Router();
 
-router.post("/tasks/add", async (req, res) => {
-    try {
-        const task = Task(req.body)
+router.get("/", renderTasks);
 
-        // Objeto que se guardara en mongodb
-        // const taskSaved = await task.save()
-        await task.save()
+router.post("/tasks/add", createTask);
 
-        // console.log(taskSaved)
-        res.redirect("/");
-    } catch (error) {
-        console.log(error);
-    }
-})
+// router.get("/about", (req, res) => {
+//     res.render("about")
+// })
 
-router.get("/about", (req, res) => {
-    res.render("about")
-})
+router.get("/edit/:id", renderTaskEdit);
 
-router.get("/edit/:id", async (req, res) => {
-    try {
-        const task = await Task.findById(req.params.id).lean(); //lean para que admita objetos de js y no de mongodb
-
-        res.render("edit", { task });
-    } catch (error) {
-        console.log(error.message);
-    }
-})
-
-// Para en el frontend, debe usarse un método que crea un input oculto y es algo más complicado para poder usar el método http put
-// se usa POST para efectos prácticos. Si hubiera un framework como react, tampoco sería necesario
-// Para recibir los datos del formulario que envíe el frontend
 // edit route
-router.post("/edit/:id", async (req, res) => {
-
-    const { id } = req.params
-
-    await Task.findByIdAndUpdate(id, req.body)
-
-    console.log(req.body);
-    console.log(req.params.id);
-
-    res.redirect("/");
-})
+router.post("/edit/:id", editTask);
 
 // delete router
-// El usuario "elimina", llega al backend, se ejecuta el siguiente código y por último, se direcciona a la misma página
-router.get("/delete/:id", async (req, res) => {
-    const { id } = req.params;
-
-    await Task.findByIdAndDelete(id); //no es necesario pasar un body porque no lo va a actualizar. Solo borrará por id
-    res.redirect("/");
-})
+router.get("/delete/:id", deleteTask);
 
 // toggleDone -> toggle = que cambie a true o false
-router.get("/toggleDone/:id", async (req, res) => {
-
-    const { id } = req.params;
-
-    const task = await Task.findById(id);
-
-    // Por defecto es false, el símbolo ! lo convierte en true
-    task.done = !task.done;
-
-    // Guardar
-    await task.save();
-
-    res.redirect("/");
-})
+router.get("/toggleDone/:id", toggleDone);
 
 export default router;
