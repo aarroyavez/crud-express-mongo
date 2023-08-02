ddcc12e02e6f1a3e1b8df4533236a767eaca4b0c
Alexander Arroyave Zapata <aarroyavez@unal.edu.co>
2023-08-02 17:52:27 -0500

    dist

--- dist/index.js
+++ dist/index.js
@@ -6,5 +6,5 @@ var _app = _interopRequireDefault(require("./app"));
 require("./database");
 // debe importarse al inicio de la app
 
-_app["default"].listen(4050);
-console.log("Server on port", 4050);
+_app["default"].listen(4060);
+console.log("Server on port", 4060);
